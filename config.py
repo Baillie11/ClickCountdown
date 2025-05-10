2ea828504012c9e3591f6215fedb876dc09d52cd
Baillie11 <andrewgbaillie@hotmail.com>
2025-05-10 17:58:40 +1000

    Updated config.py to support MongoDB Atlas via environment variable

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'yoursecretkeyhere'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/clickcountdown'
+    MONGO_URI = os.environ.get('MONGO_URI') or "mongodb+srv://andrew:aB2TizhFa2FXkF@click.ahmkf2e.mongodb.net/clickcountdown?retryWrites=true&w=majority&appName=Click"
